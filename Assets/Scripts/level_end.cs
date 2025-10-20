75adb468b94053171291f92b563ddadd55b6851a
Millie <walterdragonheart@gmail.com>
2025-10-20 20:56:15 +0100

    Added end of level detection
    Added an end of level detection block that rotates the camera to the next stage area (WIP on transitioning camera smoothly)

--- Assets/Scripts/level_end.cs
+++ Assets/Scripts/level_end.cs
@@ -5,7 +5,7 @@ public class level_end : MonoBehaviour
 {
     public GameObject cam_object;
     private camera cam_script;
-    public Quaternion cam_rotation;
+    public Vector3 cam_rotation; // We store this as a vector 3 then convert into Quaternion later so we can multiply the value for a smooth transition
     public Vector3 cam_position;
 
     private void Start()
@@ -13,10 +13,23 @@ public class level_end : MonoBehaviour
         cam_script = cam_object.GetComponent<camera>();
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
         Debug.Log("You hit the end!");
-        cam_script.camera_offset_position = cam_position;
-        cam_script.camera_offset_position = cam_position;
+
+        float currentTime = 0;
+        while (currentTime < 1)
+        {
+            var someValueFrom0To1 = currentTime / 1;
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= 1) {currentTime = 1;} // This is so the final loop ends with a x1 multiplier instead of something like 1.00168
+
+            cam_script.camera_offset_position = cam_position * currentTime;
+            cam_script.camera_offset_rotation = Quaternion.Euler(cam_rotation * currentTime);
+        }
+        
+        // cam_script.camera_offset_position = cam_position;
+         //cam_script.camera_offset_rotation = cam_rotation;
     }
 }
