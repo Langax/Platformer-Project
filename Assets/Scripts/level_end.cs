6717cf87b7e23384fe3474b6f4a860fd914195f1
Millie <walterdragonheart@gmail.com>
2025-10-24 02:07:27 +0100

    Addded level end
    Added the level end, added interpolation, fixed multiple bugs and made the level end block modular

--- Assets/Scripts/level_end.cs
+++ Assets/Scripts/level_end.cs
@@ -3,33 +3,63 @@ using System.Collections;
 
 public class level_end : MonoBehaviour
 {
-    public GameObject cam_object;
     private camera cam_script;
+    private bool interpolating = false;
+    public GameObject cam_object;
     public Vector3 cam_rotation; // We store this as a vector 3 then convert into Quaternion later so we can multiply the value for a smooth transition
     public Vector3 cam_position;
+    public bool reverse_rotation = true; // Basically should it rotate left or right when transitioning!
+    private Vector3 opposite_rotation; // We use this if reverse_rotation is set to true, if we changed cam_rotation the goal would constantly change leading to errors!
+    static float interp_duration = 3; // The amount of time the interpolation should take
+    
+    private float current_time = interp_duration; // This stores the current elapsed time of the interpolation. the interpolation is triggered by current_time being under it's "max duration"
+    private Vector3 cam_starting_position; // So we can interpolate from it's starting position
+    private Vector3 cam_starting_rotation; // Same as above for rotation
 
     private void Start()
     {
-        cam_script = cam_object.GetComponent<camera>();
+        cam_script = cam_object.GetComponent<camera>(); // So we can change the variables from the camera script
     }
+
+    private void Update()
+    {
+        if (current_time < interp_duration)
+        {
+            current_time += Time.deltaTime;
+
+            if (current_time >= interp_duration) { current_time = interp_duration; interpolating = true; } // This is so the final loop ends with a x1 multiplier instead of something like 1.00168
+
+            float interp_amount = 1/(interp_duration / current_time);  // This basically produces a value between 0 and 1 to tell what stage of the interp we are at and do maths with the positions
+
+            //cam_script.camera_offset_position =  cam_position * interp_amount;
+            cam_script.camera_offset_position = (cam_starting_position * (1 - interp_amount)) + (cam_position * interp_amount); // This is a linear interp equasion I made after little brain thinkie moment, interp amount is basically time
+
+            if (reverse_rotation == true)
+            {
+                cam_script.camera_offset_rotation = Quaternion.Euler((cam_starting_rotation * (1 - interp_amount)) - (opposite_rotation * interp_amount)); // Same equasion just on rotation, btw the 1.0f is so it's a float I was getting some odd errors
+            }
+            else
+            {
+                cam_script.camera_offset_rotation = Quaternion.Euler((cam_starting_rotation * (1 - interp_amount)) + (cam_rotation * interp_amount)); // Same equasion just on rotation, btw the 1.0f is so it's a float I was getting some odd errors
+            }   
+        }
+    } // End update
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("You hit the end!");
-
-        float currentTime = 0;
-        while (currentTime < 1)
+        if (cam_script.camera_offset_position != cam_position && interpolating == false) // checks IF it even needs to run (stops errors happening from reversing saves game lag etc), I did ORIGINALLY have a rotation check in there however Quaternion stores really weird and one of the numbers were negative making it always trigger :c
         {
-            var someValueFrom0To1 = currentTime / 1;
-            currentTime += Time.deltaTime;
+            interpolating = true;
+            cam_starting_position = cam_script.camera_offset_position; // Holds the original starting position
+            cam_starting_rotation = cam_script.camera_offset_rotation.eulerAngles; // the .eulearAngles converts it into vector3 for MATHS to happen. okay!
+            if (reverse_rotation == true) // Reverses the angles so that it goes to the targeted angle e.g. target is 270 but without this it'd move to 90
+            {
+                if (cam_rotation.x != 0) { opposite_rotation = new Vector3(360 - cam_rotation.x, cam_rotation.y, cam_rotation.z); }// Getting the inverse rotation as our goal needs to change
+                if (cam_rotation.y != 0) { opposite_rotation = new Vector3(cam_rotation.x, 360 - cam_rotation.y, cam_rotation.z); }// these all do it but it needs to check that it's not 0
+                if (cam_rotation.z != 0) { opposite_rotation = new Vector3(cam_rotation.x, cam_rotation.y, 360 - cam_rotation.z); }// as 360 - 0 = 360 NOT 0 so it'd spin all around (idk how to get the inverse as this engine doesn't have negative rotations)
+            }
+            current_time = 0; // This will start the interp
+        } 
 
-            if (currentTime >= 1) {currentTime = 1;} // This is so the final loop ends with a x1 multiplier instead of something like 1.00168
-
-            cam_script.camera_offset_position = cam_position * currentTime;
-            cam_script.camera_offset_rotation = Quaternion.Euler(cam_rotation * currentTime);
-        }
-        
-        // cam_script.camera_offset_position = cam_position;
-         //cam_script.camera_offset_rotation = cam_rotation;
-    }
-}
+    }// end collision
+}// End class / script
